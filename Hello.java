7f4c3cd4ff9ba83f48cf7337792335adc0c5860a
Martin Řezníček <martin.reznicek@digiteqautomotive.com>
2020-04-03 15:33:07 +0200

    Fixed: wrong string declaration

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, Jenkins" in the terminal window.
-      string s = "mr. Jenkins";
+      String s = "mr. Jenkins";
 	System.out.println("Hello, " + s);
 
    }
